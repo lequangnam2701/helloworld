69afc0230001deb67fa636a9a29920f9071ede61
lequangnam2701 <142825327+lequangnam2701@users.noreply.github.com>
2023-08-27 23:50:40 +0700

    Them bai tap 6

---
+++ bai tap 6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+  int a = 10;
+  float b = 24.67892345;
+  char ch = 'A';
+  
+  printf("\nI\Integer data = %d", a);
+  printf("\nFloat Data = %f", b);
+  printf("\nCharacter = %c", ch);
+  printf("\nThis prints the string");
+  printf("%s", "\nThis also prints a string");
+  	
+ } 
